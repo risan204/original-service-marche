8443ed249ebc93dad6e7e97838c1f68193734b82
rie.nakagawa <rsrsrs.0518@aol.com>
2021-08-02 14:06:20 +0000

    user login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,13 @@
 
 @section('content')
     <div class="text-center">
-        <h1>会員登録</h1>
+        <h1>ログイン</h1>
     </div>
 
     <div class="row">
         <div class="col-sm-6 offset-sm-3">
 
-            {!! Form::open(['route' => 'signup.post']) !!}
-                <div class="form-group">
-                    {!! Form::label('name', '名前') !!}
-                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-                </div>
-
+            {!! Form::open(['route' => 'login.post']) !!}
                 <div class="form-group">
                     {!! Form::label('email', 'メールアドレス') !!}
                     {!! Form::email('email', null, ['class' => 'form-control']) !!}
@@ -24,13 +19,11 @@
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
 
-                <div class="form-group">
-                    {!! Form::label('password_confirmation', 'パスワード(確認)') !!}
-                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
-                </div>
-
-                {!! Form::submit('登録する', ['class' => 'btn btn-primary btn-block']) !!}
+                {!! Form::submit('Log in', ['class' => 'btn btn-primary btn-block']) !!}
             {!! Form::close() !!}
+
+            {{-- ユーザ登録ページへのリンク --}}
+            <p class="mt-2"> {!! link_to_route('signup.get', '会員登録はこちら') !!}</p>
         </div>
     </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Vegetable Marche</h1>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Vegetable Marche！</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'ログイン', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
