2465b00ee0230e64cd4fd1713e452d365a51a5e6
rie.nakagawa <rsrsrs.0518@aol.com>
2021-08-24 10:56:18 +0000

    item

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -3,43 +3,45 @@
 @section('content')
 
     <h1>商品を出品する</h1>
-
-    <div class="row">
+    <br>
+    
         {{-- 商品登録ページへのリンク --}}
-        <div class="col-6">
-                <form action="{{ action('ItemsController@store') }}" method="post" enctype="multipart/form-data">
+                <div class="form-group row">
+                    <div class="col-sm-3">{!! Form::label('file', '商品写真添付:') !!}</div>
+                    
+                <div class="col-sm-9"><form action="{{ action('ItemsController@store') }}" method="post" enctype="multipart/form-data">
                 <!-- アップロードフォームの作成 -->
                 <input type="file" name="file">
+                </div>
+                </div>
                 @csrf
-                <br>
-                <div class="form-group">     　　
-                    {!! Form::label('name', '商品名:') !!}
-                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                <div class="form-group row">
+                    <div class="col-sm-3">{!! Form::label('name', '商品名:') !!}</div>
+                    <div class="col-sm-9">{!! Form::text('name', null, ['class' => 'form-control']) !!}</div>
+                </div>               
+                <div class="form-group row">
+                    <div class="col-sm-3">{!! Form::label('price', '金額:') !!}</div>
+                    <div class="col-sm-9">{!! Form::text('price', null, ['class' => 'form-control']) !!}</div>
                 </div>
-                <div class="form-group">
-                    {!! Form::label('price', '金額:') !!}
-          {!! Form::text('price', null, ['class' => 'form-control']) !!}
-                     
+                <div class="form-group row">
+                    <div class="col-sm-3">{!! Form::label('size', 'サイズ:') !!}</div>
+                    <div class="col-sm-9">{!! Form::text('size', null, ['class' => 'form-control']) !!}</div>
                 </div>
-                <div class="form-group">
-                    {!! Form::label('size', 'サイズ:') !!}
-                    {!! Form::text('size', null, ['L' => 'Large', 'M' => 'Midium', 'S' => 'Small']) !!}
+                <div class="form-group row">
+                    <div class="col-sm-3">{!! Form::label('quantity', '数量:') !!}</div>
+                    <div class="col-sm-9">{!! Form::number('quantity', null, ['class' => 'form-control']) !!}</div>
                 </div>
-                <div class="form-group">
-                    {!! Form::label('quantity', '数量:') !!}
-                    {!! Form::number('quantity', null, ['class' => 'form-control']) !!}
+                <div class="form-group row">
+                    <div class="col-sm-3">{!! Form::label('area', '産地:') !!}</div>
+                    <div class="col-sm-9">{!! Form::text('area', null, ['class' => 'form-control']) !!}</div>
                 </div>
-                <div class="form-group">
-                    {!! Form::label('area', '産地:') !!}
-                    {!! Form::text('area', null, ['class' => 'form-control']) !!}
+                <div class="form-group row">
+                    <div class="col-sm-3">{!! Form::label('stock', '在庫:') !!}</div>
+                    <div class="col-sm-9">{!! Form::number('stock', null, ['class' => 'form-control']) !!}</div>
                 </div>
-                <div class="form-group">
-                    {!! Form::label('stock', '在庫:') !!}
-                    {!! Form::number('stock', null, ['class' => 'form-control']) !!}
-                </div>
-                {!! Form::submit('出品する', ['class' => 'btn btn-secondary btn-block']) !!}
+                <div class="offset-3 col-sm-9">{!! Form::submit('出品する', ['class' => 'btn btn-secondary btn-block']) !!}</div>
             {!! Form::close() !!}
             </div>
-            </div>
-    </div>
+           
+    
 @endsection
